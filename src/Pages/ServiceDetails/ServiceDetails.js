1264b090609a390c90e12dd106780efc7c69ea0f
Nahid-Hasan_Sourav <nahidhasansourav05@gmail.com>
2022-11-09 21:15:44 +0600

    modal added succesfully. when user will be login if he/she wants to send review about any product that's time he can be see a modal that will collect his/her review otherwise there will be a modal that will show for login

--- src/Pages/ServiceDetails/ServiceDetails.js
+++ src/Pages/ServiceDetails/ServiceDetails.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, Container, Row} from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
-
+import { useContext } from 'react';
 import Form from "react-bootstrap/Form";
+import { AuthContext } from '../../Contexts/AuthProvider/AuthProvider';
+
+import Nav from 'react-bootstrap/Nav';
+
+import Modal from 'react-bootstrap/Modal';
+
 
 
 const ServiceDetails = () => {
+  const [show, setShow] =useState(false)
+  const handleClose = () => setShow(false);
+  const handleShow = () => {
+    setShow(true)
+    // if(user.uid){
+    //   setShow(true)
+    // }
+  };
     const services=useLoaderData()
+    const {user}=useContext(AuthContext)
+
     return (
       <Container>
         <Row className="mx-0 justify-content-center">
@@ -42,27 +58,73 @@ const ServiceDetails = () => {
           </Col>
         </Row>
         <hr></hr>
+
         <Row className="mx-0 justify-content-center">
           <Col lg={8}>
-            <h4>Service Review</h4>
-            <Form>
-              <FloatingLabel controlId="floatingTextarea2" label="type here...">
-                <Form.Control
-                  as="textarea"
-                  placeholder=""
-                  style={{ height: "100px" }}
-                />
-              </FloatingLabel>
+            <Button
+              variant="primary"
+              type="submit"
+              className="w-100 fw-bold my-3"
+              onClick={handleShow}
+            >
+              SEND REVIEW
+            </Button>
+            {/* if user is logged in this modal will show to get user review  */}
+            {user?.uid ? (
+              <>
+                <Modal show={show} onHide={handleClose} animation={false}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Add Your Review</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    <Form>
+                      <FloatingLabel
+                        controlId="floatingTextarea2"
+                        label="type here..."
+                      >
+                        <Form.Control
+                          as="textarea"
+                          placeholder=""
+                          style={{ height: "100px" }}
+                        />
+                      </FloatingLabel>
 
-              <Button
-                variant="primary"
-                type="submit"
-                className="w-100 fw-bold my-3"
-              >
-                SEND REVIEW
-              </Button>
-            </Form>
+                      
+                    </Form>
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="primary" type='submit'>
+                      Send Review
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
+              </>
+            ) : (
+              <>
+                {/* if user is not logged in this modal will show to log in */}
+                <Modal show={show} onHide={handleClose} animation={false}>
+                 
+                  <Modal.Body>
+
+                  Sorry!! You are Not Loggedin.
+                  If You want to add Review About This Service You must need to Login<Nav.Link  className='my-2 text-white btn btn-danger' as={Link} to='/login'>Login</Nav.Link>
+
+
+
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                      Close
+                    </Button>
+                    
+                  </Modal.Footer>
+                </Modal>
+              </>
+            )}
           </Col>
+        </Row>
+        <Row className="mx-0">
+
         </Row>
       </Container>
     );
