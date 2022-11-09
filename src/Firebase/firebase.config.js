78fc2ee08745f00d5344c9839f7ebcbf4d6eb6f5
Nahid-Hasan_Sourav <nahidhasansourav05@gmail.com>
2022-11-09 18:20:51 +0600

    solve error in environment variable

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -4,13 +4,16 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// AIzaSyBBHpOHNLxQRsOoB0E3snm3PA_qhswjFsQ
+// AIzaSyBBHpOHNLxQRsOoB0E3snm3PA_qhswjFsQ
+// AIzaSyBBHpOHNLxQRsOoB0E3snm3PA_qhswjFsQ
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
   projectId: process.env.REACT_APP_projectId,
-  storageBucket:process.env.REACT_APP_storageBucket,
+  storageBucket: process.env.REACT_APP_storageBuck,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId
+  appId: process.env.appId
 };
 
 // Initialize Firebase
